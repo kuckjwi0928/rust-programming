e06e90b12003bda256fc0f6baa940ef7f33082e8
kuckjwi <rnrghks09@gmail.com>
2023-01-30 14:40:19 +0900

    end of chapter6

---
+++ chapter6/6-3.rs
@@ -0,0 +1,23 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {
+    let penny = Coin::Penny;
+    let nickel = Coin::Nickel;
+    let dime = Coin::Dime;
+    let quarter = Coin::Quarter;
+    println!("penny: {}, nickel: {}, dime: {}, quarter: {}", value_in_cents(penny), value_in_cents(nickel), value_in_cents(dime), value_in_cents(quarter));
+}
---
+++ chapter6/6-4.rs
@@ -0,0 +1,31 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
+
+fn main() {
+    let penny = Coin::Penny;
+    let nickel = Coin::Nickel;
+    let dime = Coin::Dime;
+    let quarter = Coin::Quarter(UsState::Alabama);
+    println!("penny: {}, nickel: {}, dime: {}, quarter: {}", value_in_cents(penny), value_in_cents(nickel), value_in_cents(dime), value_in_cents(quarter));
+}
